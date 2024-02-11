a4148395e3bcdd0708d401114855fa1117ef12f2
Ankitverma870 <ankitverma0288@gmail.com>
2024-02-12 00:38:41 +0530

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../component/Layout/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+        <h1>home page</h1>
+    </Layout>
+  )
+}
+
+export default Home
